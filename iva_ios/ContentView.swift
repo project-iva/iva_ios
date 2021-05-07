a8a6f12e79aafce824ab593ad358626717672ecf
Igor Pidik <IgorPidik@gmail.com>
2021-05-07 18:25:39 +0200

    Initial Commit

---
+++ iva_ios/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  iva_ios
+//
+//  Created by Igor Pidik on 07/05/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ iva_ios/iva_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  iva_iosApp.swift
+//  iva_ios
+//
+//  Created by Igor Pidik on 07/05/2021.
+//
+
+import SwiftUI
+
+@main
+struct iva_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
